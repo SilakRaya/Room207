72ab29eab8af414a35c2813d594900c66fac2176
TomanoJuice <iandaniel30009@gmail.com>
2026-05-20 11:06:23 +0800

    Exploring the bush

---
+++ room-207-prototype/EpisodeOne/ASSets/bush.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+func _on_bush_area_1_body_entered(jolina):
+	print("Jolina has entered the bush")
+	
+func _on_bush_area_2_body_entered(jolina):
+	print("Jolina has entered a bush again")
